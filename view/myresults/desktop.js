e19a1efdaaaed80bda076e15b0633395ad6ebad0
caH40 <kmvsancho@gmail.com>
2022-10-21 14:55:24 +0300

    fixed the bug with main menu

--- view/myresults/desktop.js
+++ view/myresults/desktop.js
@@ -1,12 +1,8 @@
-import { clearCharts, mainMenuKeyboard } from '../../keyboard/keyboard.js';
+import { clearCharts } from '../../keyboard/keyboard.js';
 import { viewDesktop } from '../generate/my-results.js';
 
 export async function myResultsViewDes(ctx, myResults, title) {
 	try {
-		await ctx.editMessageText(
-			`❗<b>Главное меню. Выбор основных функций.</b>❗\n<i>main</i>`,
-			await mainMenuKeyboard(ctx)
-		);
 		await ctx
 			.replyWithHTML('<pre>' + viewDesktop(myResults, title) + '</pre>', clearCharts)
 			.then(message => ctx.session.data.messagesIdForDelete.push(message.message_id));
--- view/myresults/mobile.js
+++ view/myresults/mobile.js
@@ -1,12 +1,8 @@
-import { clearCharts, mainMenuKeyboard } from '../../keyboard/keyboard.js';
+import { clearCharts } from '../../keyboard/keyboard.js';
 import { viewMobile } from '../generate/my-results.js';
 
 export async function myResultsViewMob(ctx, myResults, title) {
 	try {
-		await ctx.editMessageText(
-			`❗<b>Главное меню. Выбор основных функций.</b>❗\n<i>main</i>`,
-			await mainMenuKeyboard(ctx)
-		);
 		await ctx
 			.replyWithHTML('<pre>' + viewMobile(myResults, title) + '</pre>', clearCharts)
 			.then(message => ctx.session.data.messagesIdForDelete.push(message.message_id));
