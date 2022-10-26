c6ec6b7692ac9b84f2f713b111a6c8895212034a
caH40 <kmvsancho@gmail.com>
2022-10-26 10:17:47 +0300

    fixed bugs with genegar mobile view

--- view/series_general/desktop.js
+++ view/series_general/desktop.js
@@ -1,65 +1,9 @@
-import { clearCharts, mainMenuKeyboard } from '../../keyboard/keyboard.js';
-import { Result } from '../../Model/Result.js';
-import { Series } from '../../Model/Series.js';
-import { Stage } from '../../Model/Stage.js';
+import { clearCharts } from '../../keyboard/keyboard.js';
 import { divisionChart } from '../../utility/chart-division.js';
 import { viewDesktop } from '../generate/general.js';
 
-export async function resultsSeriesGeneralDes(ctx, category, seriesId) {
+export async function resultsSeriesGeneralDes(ctx, resultsGeneral, category, name, type) {
 	try {
-		await ctx.editMessageText(
-			`❗<b>Главное меню. Выбор основных функций.</b>❗\n<i>main</i>`,
-			await mainMenuKeyboard(ctx)
-		);
-		const stagesDB = await Stage.find({ seriesId, hasResults: true });
-		const seriesIds = stagesDB.map(stage => stage._id);
-
-		let resultsSeries = [];
-		const lengthSeries = seriesIds.length;
-		for (let index = 0; index < lengthSeries; index++) {
-			let results = await Result.find({ stageId: seriesIds[index] }).populate('riderId');
-			resultsSeries = [...resultsSeries, ...results];
-		}
-
-		// сначала необходимо найти все элементы с уникальными именами
-		let zwiftRiderIds = new Set();
-		let points = 0;
-		resultsSeries.forEach(elm => zwiftRiderIds.add(elm.zwiftRiderId));
-
-		let resultsGeneral = [];
-		zwiftRiderIds.forEach(zwiftRiderId => {
-			points = 0;
-			resultsSeries.forEach(elm => {
-				if (zwiftRiderId === elm.zwiftRiderId) points += elm.pointsStage;
-			});
-			resultsGeneral.push({ zwiftRiderId, pointsGeneral: points });
-		});
-		//в будущем брать данные по группе и команде из коллекции Riders
-
-		resultsGeneral = resultsGeneral.map(rider => {
-			const categoryFilter = resultsSeries.find(elm => elm.zwiftRiderId === rider.zwiftRiderId);
-
-			const categoryNew = categoryFilter.riderId
-				? categoryFilter.riderId?.category
-				: categoryFilter.categoryCurrent;
-			return {
-				zwiftRiderId: rider.zwiftRiderId,
-				pointsGeneral: rider.pointsGeneral,
-				name: resultsSeries.find(elm => elm.zwiftRiderId === rider.zwiftRiderId).name,
-				category: categoryNew,
-				team: resultsSeries.find(elm => elm.zwiftRiderId === rider.zwiftRiderId).team ?? '',
-			};
-		});
-
-		const { name, type } = await Series.findOne({ _id: seriesId });
-
-		resultsGeneral = resultsGeneral
-			.filter(rider => rider.category === category)
-			.sort((a, b) => b.pointsGeneral - a.pointsGeneral);
-		resultsGeneral.forEach((rider, index) => {
-			rider.place = index + 1;
-		});
-
 		const title = `${name}, ${type}, Генеральный зачет, Категория "${category}"`;
 
 		const charts = divisionChart(resultsGeneral);
--- view/series_general/mobile.js
+++ view/series_general/mobile.js
@@ -1,59 +1,10 @@
-import { clearCharts, mainMenuKeyboard } from '../../keyboard/keyboard.js';
-import { Result } from '../../Model/Result.js';
-import { Series } from '../../Model/Series.js';
-import { Stage } from '../../Model/Stage.js';
+import { clearCharts } from '../../keyboard/keyboard.js';
 import { divisionChart } from '../../utility/chart-division.js';
 import { viewMobile } from '../generate/general.js';
 
-export async function resultsSeriesGeneralMob(ctx, category, seriesId) {
+export async function resultsSeriesGeneralMob(ctx, resultsGeneral, category, name, type) {
 	try {
-		await ctx.editMessageText(
-			`❗<b>Главное меню. Выбор основных функций.</b>❗\n<i>main</i>`,
-			await mainMenuKeyboard(ctx)
-		);
-		const stagesDB = await Stage.find({ seriesId, hasResults: true });
-		const seriesIds = stagesDB.map(stage => stage._id);
-
-		let resultsSeries = [];
-		const lengthSeries = seriesIds.length;
-		for (let index = 0; index < lengthSeries; index++) {
-			let results = await Result.find({ stageId: seriesIds[index] });
-			resultsSeries = [...resultsSeries, ...results];
-		}
-		// сначала необходимо найти все элементы с уникальными именами
-		let set = new Set();
-		let points = 0;
-		resultsSeries.forEach(elm => set.add(elm.name));
-
-		let resultsGeneral = [];
-		set.forEach(name => {
-			points = 0;
-			resultsSeries.forEach(elm => {
-				if (name === elm.name) points += elm.pointsStage;
-			});
-			resultsGeneral.push({ name, pointsGeneral: points });
-		});
-		//в будущем брать данные по группе и команде из коллекции Riders
-
-		resultsGeneral = resultsGeneral.map(rider => {
-			return {
-				name: rider.name,
-				pointsGeneral: rider.pointsGeneral,
-				category: resultsSeries.find(elm => elm.name === rider.name).category,
-				team: resultsSeries.find(elm => elm.name === rider.name).team ?? '',
-			};
-		});
-
-		const { name, type } = await Series.findOne({ _id: seriesId });
-
-		resultsGeneral = resultsGeneral
-			.filter(rider => rider.category === category)
-			.sort((a, b) => b.pointsGeneral - a.pointsGeneral);
-		resultsGeneral.forEach((rider, index) => {
-			rider.place = index + 1;
-		});
-
-		const title = `${name},Ген. зачет,"${category}"`;
+		const title = `${name}, Ген.зачет,"${category}"`;
 
 		const charts = divisionChart(resultsGeneral);
 
